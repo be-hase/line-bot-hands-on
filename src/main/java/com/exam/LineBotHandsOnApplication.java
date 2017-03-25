165be61ee697d1e649fa9348be3edd08d5df1bed
Ryosuke Hasebe <hsb.1014@gmail.com>
2017-03-26 08:57:56 +0900

    init

---
+++ src/main/java/com/exam/LineBotHandsOnApplication.java
@@ -0,0 +1,12 @@
+package com.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineBotHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBotHandsOnApplication.class, args);
+	}
+}

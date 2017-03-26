2bd2bba688a476e41fae4fb3ad429f7f8b8228df
Ryosuke Hasebe <hsb.1014@gmail.com>
2017-03-26 09:29:24 +0900

    -

--- src/main/java/com/exam/LineBotHandsOnApplication.java
+++ src/main/java/com/exam/LineBotHandsOnApplication.java
@@ -2,11 +2,19 @@ package com.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LineBotHandsOnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LineBotHandsOnApplication.class, args);
-	}
+    @RequestMapping("/webhook")
+    public String helloWorld() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineBotHandsOnApplication.class, args);
+    }
 }
